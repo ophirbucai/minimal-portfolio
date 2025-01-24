5d2b0035b140163e90a1a5da17347e381b8000f6
Ophir Bucai <ophirbucai@gmail.com>
2025-01-24 19:16:17 +0200

    Test setup, handle unresolved context

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react";
 
+import { ThemeProvider } from "@/context/theme";
 import App from "./app";
 
 describe("App", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>,
+    );
     expect(baseElement).toBeTruthy();
   });
 });
---
+++ src/setupTests/_setup_matchMedia.ts
@@ -0,0 +1,14 @@
+import { beforeAll, vi } from "vitest";
+
+beforeAll(() => {
+  window.matchMedia = vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }));
+});
