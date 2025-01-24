403f5af35c311cbcc37033634be981dc5fd153da
Ophir Bucai <ophirbucai@gmail.com>
2025-01-24 06:40:22 +0200

    Move hooks to theme provider

--- src/context/theme/theme-context.provider.tsx
+++ src/context/theme/theme-context.provider.tsx
@@ -1,40 +1,27 @@
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { createContext, useCallback, useContext, useEffect, useRef, useState } from "react";
 import { ThemeState } from "./theme-context.state";
-import type { ThemeType } from "./theme-context.types";
+import type { SystemThemeType, ThemeType } from "./theme-context.types";
 
 interface ThemeContextType {
-  themeState: ThemeState;
+  theme: ThemeType;
+  systemTheme: SystemThemeType;
+  selectTheme: (newTheme: ThemeType) => void;
+  toggleTheme: () => void;
+  nextTheme: SystemThemeType;
 }
 
 const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const themeState = useRef(new ThemeState()).current;
+  const [theme, setTheme] = useState(themeState.theme);
+  const [systemTheme, setSystemTheme] = useState(themeState.systemTheme);
+  const [nextTheme, setNextTheme] = useState(themeState.nextTheme);
 
   useEffect(() => {
     themeState.applyTheme();
     return () => themeState.cleanup();
   }, [themeState]);
-
-  return (
-    <ThemeContext.Provider value={{ themeState }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
-
-export const useTheme = () => {
-  const { themeState } = useContext(ThemeContext);
-  const [theme, setTheme] = useState(themeState.theme);
-  const [systemTheme, setSystemTheme] = useState(themeState.systemTheme);
-  const [nextTheme, setNextTheme] = useState(themeState.nextTheme);
 
   useEffect(() => {
     themeState.subscribe(() => {
@@ -55,11 +42,19 @@ export const useTheme = () => {
     themeState.toggleTheme();
   }, [themeState]);
 
-  return {
-    theme,
-    systemTheme,
-    selectTheme,
-    toggleTheme,
-    nextTheme,
-  };
+  return (
+    <ThemeContext.Provider value={{ theme, nextTheme, systemTheme, selectTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const themeContext = useContext(ThemeContext);
+
+  if (!themeContext) {
+    throw new Error("useTheme must be used within ThemeContext Provider");
+  }
+
+  return themeContext;
 };
