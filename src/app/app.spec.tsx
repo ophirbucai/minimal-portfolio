e30c14f6d3756a3afa7770f595a8a94dfb5f8a11
Ophir Bucai <ophirbucai@gmail.com>
2025-01-28 18:32:57 +0200

    Missing Dialog in CI test fixed

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -2,12 +2,15 @@ import { render } from "@testing-library/react";
 
 import { ThemeProvider } from "@/context/theme";
 import App from "./app";
+import { Dialog } from "@radix-ui/react-dialog";
 
 describe("App", () => {
   it("should render successfully", () => {
     const { baseElement } = render(
       <ThemeProvider>
-        <App />
+        <Dialog>
+          <App />
+        </Dialog>
       </ThemeProvider>,
     );
     expect(baseElement).toBeTruthy();
